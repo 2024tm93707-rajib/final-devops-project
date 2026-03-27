fed0210761b16ce81b1095a80c4d2cebd452e915
2024tm93707-rajib <2024tm93707@wilp.bits-pilani.ac.in>
2026-03-27 11:49:07 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,6 +1,26 @@
-from app import create_app
+from flask import Flask, jsonify, request
+from fitness_service import calculate_bmi
 
-app = create_app()
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def home():
+    return jsonify({
+        "service": "ACEest Fitness API",
+        "status": "running"
+    })
+
+@app.route("/bmi", methods=["POST"])
+def bmi():
+    data = request.json
+    weight = data.get("weight")
+    height = data.get("height")
+
+    bmi_value = calculate_bmi(weight, height)
+
+    return jsonify({
+        "bmi": bmi_value
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
